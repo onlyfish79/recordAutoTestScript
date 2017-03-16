f662499eb169f976e34c93f029b64b9f79bf4ec1
jinfeng <tianjinfeng@letv.com>
2017-03-16 14:31:00 +0800

    1. support get position in landscape and portrait; 2. generate simple test script

--- getPosition.py
+++ getPosition.py
@@ -2,11 +2,11 @@
 # encoding: utf-8
 
 import time
-import sys
 import signal
 import numpy
 import collections
 import os
+from libs.GetDeviceInfo import getDeviceInfo
 from subprocess import PIPE, Popen
 from threading  import Thread
 try:
@@ -14,25 +14,13 @@ try:
 except ImportError:
     from queue import Queue, Empty  # python 3.x
 
-#ON_POSIX = 'posix' in sys.builtin_module_namess
-ON_POSIX = True
-
-MAXWAITTIME=300
 finishFlag = False
-def timer(proc):
-    global finishFlag
-    count = MAXWAITTIME
-    while count > 0:
-        if finishFlag is True:
-            break
-        count -= 1
-        time.sleep(1)
-        print 'sleep %d' % (MAXWAITTIME-count)
+deviceName = '5LM7N16224000261'
+currPath = os.getcwd()
+scriptFileRoot = os.path.join(currPath, 'scriptFile')
 
-    finishFlag = True
-    #proc.kill()
-    #proc.wait()
-    print 'end of timer, finishFlag is %d, kill adb shell getevent process.......' % finishFlag
+if os.path.isdir(scriptFileRoot) is False:
+    os.makedirs(scriptFileRoot)
 
 def signal_handler(signal, frame):
     global finishFlag
@@ -46,11 +34,35 @@ def enqueue_output(out, queue):
             queue.put(line)
     out.close()
 
+def check_portrait_landscape():
+    checkCmd = "adb -s %s shell dumpsys input | grep 'SurfaceOrientation'  | awk '{ print $2 }' | head -n 1" % deviceName
+    result = os.popen(checkCmd , 'r')
+    content = result.read()
+    result.close()
+    screenTypeValue = int(content.replace('\r\n', ''))
+    screenType = 'portrait'  #默认竖屏
+    if screenTypeValue == 0 or screenTypeValue == 2:
+        screenType = 'portrait'   #竖屏
+    elif screenTypeValue == 1 or screenTypeValue == 3:
+        screenType = 'landscape'  #横屏
+    return screenType
+
+def get_resolution():
+    deviceInfo = getDeviceInfo(deviceName)
+    resolutioin = deviceInfo['deviceResolution']
+    splitInfo = resolutioin.split('x')
+    max_width = int(splitInfo[0].split()[0])
+    max_height = int(splitInfo[1].split()[0])
+    return (max_width, max_height)
+
+
 def getevent_position():
-    geteventCmd = 'adb shell getevent -lt /dev/input/event5'
+    if cmp(deviceName , '5LM7N16224000261') == 0:
+        geteventCmd = 'adb shell getevent -lt /dev/input/event5'  #华为Mate8
+    elif cmp(deviceName, '63a9bca7') == 0:
+        geteventCmd = 'adb shell getevent -lt /dev/input/event4'   #vivo
     p = Popen(geteventCmd, shell=True, stdout=PIPE)
     print p.pid
-    #thread.start_new_thread(timer, (proc, ))
     positionRecord = collections.OrderedDict()
     q = Queue()
     t = Thread(target=enqueue_output, args=(p.stdout, q))
@@ -59,6 +71,9 @@ def getevent_position():
 
     noOutput = False
     hasLine = False
+    screenType = check_portrait_landscape()
+    resolution = get_resolution()
+    max_width = resolution[0]
     while noOutput is False or hasLine is False:
         if finishFlag is True:
             break
@@ -79,21 +94,34 @@ def getevent_position():
             positionRecord[timeValue].append(positionValue)
     p.kill()
     p.wait()
-    checkGetEventCmd = 'ps aux | grep getevent'
-    os.system(checkGetEventCmd)
-    print '**********position len is %d' % len(positionRecord)
+    #checkGetEventCmd = 'ps aux | grep getevent'
+    #os.system(checkGetEventCmd)
     clickOp = collections.OrderedDict()
     if len(positionRecord) == 1:
         startTime = positionRecord.keys()[0]
+        positionValue = positionRecord[startTime]
+        if cmp(screenType, 'landscape') == 0:
+            landscape_x = positionValue[1]
+            landscape_y = max_width - positionValue[0]
+            positionValue = [landscape_x, landscape_y]
         clickOp['clickType'] = 'tap'
         clickOp['clickStartTime'] = startTime
-        clickOp['clickPosition'] = positionRecord[startTime]
+        clickOp['clickPosition'] = positionValue
+        clickOp['screenType'] = screenType
         print clickOp
     elif len(positionRecord) > 1:
         startTime = positionRecord.keys()[0]
         startPosition = positionRecord[startTime]
+        if cmp(screenType, 'landscape') == 0:
+            landscape_x = startPosition[1]
+            landscape_y = max_width - startPosition[0]
+            startPosition = [landscape_x, landscape_y]
         endTime = positionRecord.keys()[-1]
         endPosition = positionRecord[endTime]
+        if cmp(screenType, 'landscape') == 0:
+            landscape_x = endPosition[1]
+            landscape_y = max_width - endPosition[0]
+            endPosition = [landscape_x, landscape_y]
         startVect = numpy.array(startPosition)
         endVect = numpy.array(endPosition)
         eucDist = round(numpy.linalg.norm(endVect-startVect), 3)
@@ -101,6 +129,7 @@ def getevent_position():
             clickOp['clickType'] = 'tap'
             clickOp['clickStartTime'] = startTime
             clickOp['clickPosition'] = startPosition
+            clickOp['screenType'] = screenType
         else:
             clickOp['clickType'] = 'swipe'
             clickOp['clickStartTime'] = startTime
@@ -108,43 +137,73 @@ def getevent_position():
             clickOp['clickStartPosition'] = startPosition
             clickOp['clickEndPosition'] = endPosition
             clickOp['eucDist'] = eucDist
+            clickOp['screenType'] = screenType
         print clickOp
     print 'end of getevent position....'
     return clickOp
 
+def write_constant(scriptFile):
+    scriptFile.write('#!/usr/bin/env python\r')
+    scriptFile.write('# encoding: utf-8\r')
+    scriptFile.write('\r')
+    scriptFile.write('import os\r')
+    scriptFile.write('import time\r')
+    scriptFile.write('\r')
+    scriptFile.write('if __name__ == "__main__":\r')
 
-signal.signal(signal.SIGINT, signal_handler)
-#clickRecord = collections.OrderedDict()
-clickRecord = []
-clickNum = 0
-while finishFlag is False:
-    clickNum += 1
-    #clickRecord[clickNum] = getevent_position()
-    recordValue = getevent_position()
-    if len(recordValue) > 0:
-        clickRecord.append(recordValue)
-        print '*********第%d次点击' % clickNum
+def generate_script():
+    signal.signal(signal.SIGINT, signal_handler)
+    clickRecord = []
+    clickNum = 0
+    while finishFlag is False:
+        clickNum += 1
+        #clickRecord[clickNum] = getevent_position()
+        recordValue = getevent_position()
+        if len(recordValue) > 0:
+            clickRecord.append(recordValue)
+            print '*********第%d次点击' % clickNum
 
-tapNum = 0
-swipeNum = 0
-clickNum = 0
-preClickTime = 0
-for record in clickRecord:
-    clickNum += 1
-    clickType = record['clickType']
-    clickTime = record['clickStartTime']
-    if clickNum > 1:
-        diffTime = round(float(clickTime)-float(preClickTime), 1)
-        print '第%d次点击是: %s, 与第%d时间间隔是%s' % (clickNum, clickType, clickNum - 1, str(diffTime))
-    else:
-        print '第%d次点击是: %s' % (clickNum, clickType)
+    tapNum = 0
+    swipeNum = 0
+    clickNum = 0
+    preClickTime = 0
+    scriptFilePath = os.path.join(scriptFileRoot, '%s-testScript.py' % deviceName)
+    scriptFile = open(scriptFilePath, 'w')
+    write_constant(scriptFile)
 
-    if clickType == 'tap':
-        tapNum += 1
-    elif clickType == 'swipe':
-        swipeNum += 1
-    preClickTime = clickTime
-print 'click次数是：%d, 其中tap 次数是: %d, swipe次数是%d' % (clickNum, tapNum, swipeNum)
+    for record in clickRecord:
+        clickNum += 1
+        clickType = record['clickType']
+        clickTime = record['clickStartTime']
+        diffTime = 0
+        if clickNum > 1:
+            diffTime = round(float(clickTime)-float(preClickTime), 1)
+
+        if clickType == 'tap':
+            print '第%d次点击是: %s, 屏幕状态: %s, 坐标是: %s, 与第%d时间间隔是%s' % (clickNum, clickType, record['screenType'], str(record['clickPosition']), clickNum - 1, str(diffTime))
+            if diffTime > 0:
+                content = '    time.sleep(%s)\r' % str(diffTime)
+                scriptFile.write(content)
+            content = "    os.system('adb -s %s shell input tap %d %d')\r"  % (deviceName, record['clickPosition'][0], record['clickPosition'][1])
+            scriptFile.write(content)
+            tapNum += 1
+        elif clickType == 'swipe':
+            print '第%d次点击是: %s, 屏幕状态: %s, 坐标是: %s%s, 与第%d时间间隔是%s' % (clickNum, clickType, record['screenType'], str(record['clickStartPosition']), str(record['clickEndPosition']), clickNum - 1, str(diffTime))
+            if diffTime > 0:
+                content = '    time.sleep(%s)\r' % str(diffTime)
+                scriptFile.write(content)
+            content = "    os.system('adb -s %s shell input swipe %d %d %d %d')\r" % (deviceName, record['clickStartPosition'][0], record['clickStartPosition'][1], \
+                                    record['clickEndPosition'][0], record['clickEndPosition'][1])
+            scriptFile.write(content)
+            swipeNum += 1
+        preClickTime = clickTime
+    content = "    os.system('adb -s %s shell pm clear com.example.TestPlugin')\r" % deviceName
+    scriptFile.write(content)
+    scriptFile.write(content)
+    scriptFile.write("    print 'end of test script.......'\r")
+    scriptFile.close()
+    print 'click次数是：%d, 其中tap 次数是: %d, swipe次数是%d' % (clickNum, tapNum, swipeNum)
 
 
-
+if __name__ == '__main__':
+    generate_script()
