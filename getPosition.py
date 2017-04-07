9357709f8d649b014418d0ab37917a3b0c2b27e6
jinfeng <tianjinfeng@letv.com>
2017-04-07 10:40:31 +0800

    1. bugfix generate simple test script; 2. save click position

--- getPosition.py
+++ getPosition.py
@@ -6,6 +6,7 @@ import signal
 import numpy
 import collections
 import os
+import subprocess
 from libs.GetDeviceInfo import getDeviceInfo
 from subprocess import PIPE, Popen
 from threading  import Thread
@@ -14,10 +15,14 @@ try:
 except ImportError:
     from queue import Queue, Empty  # python 3.x
 
+
 finishFlag = False
-deviceName = '5LM7N16224000261'
+deviceName = '5LM7N16224000261' #hw Mate8
+#deviceName = 'KWG5T17105003967'  # hw P9
+#deviceName = 'FA5B3BJ01146'    #htc
+#deviceName = 'R4WG45TCUCUCVGKN'  #oppo
 currPath = os.getcwd()
-scriptFileRoot = os.path.join(currPath, 'scriptFile')
+scriptFileRoot = os.path.join(currPath, 'scriptFile', deviceName)
 
 if os.path.isdir(scriptFileRoot) is False:
     os.makedirs(scriptFileRoot)
@@ -39,12 +44,16 @@ def check_portrait_landscape():
     result = os.popen(checkCmd , 'r')
     content = result.read()
     result.close()
-    screenTypeValue = int(content.replace('\r\n', ''))
     screenType = 'portrait'  #默认竖屏
-    if screenTypeValue == 0 or screenTypeValue == 2:
-        screenType = 'portrait'   #竖屏
-    elif screenTypeValue == 1 or screenTypeValue == 3:
-        screenType = 'landscape'  #横屏
+    print 'check portrait result is: %s' % repr(content)
+    if len(content) > 0:
+        screenTypeValue = int(content.replace('\r\n', ''))
+        if screenTypeValue == 0 or screenTypeValue == 2:
+            screenType = 'portrait'   #竖屏
+        elif screenTypeValue == 1 or screenTypeValue == 3:
+            screenType = 'landscape'  #横屏
+        else:
+            print 'dumpsys input has problem.......'
     return screenType
 
 def get_resolution():
@@ -56,11 +65,11 @@ def get_resolution():
     return (max_width, max_height)
 
 
-def getevent_position():
-    if cmp(deviceName , '5LM7N16224000261') == 0:
-        geteventCmd = 'adb shell getevent -lt /dev/input/event5'  #华为Mate8
+def getevent_position(resolution):
+    if cmp(deviceName , '5LM7N16224000261') == 0 or cmp(deviceName, 'R4WG45TCUCUCVGKN') == 0:
+        geteventCmd = 'adb -s %s shell getevent -lt /dev/input/event5' % deviceName  #华为Mate8, oppo
     elif cmp(deviceName, '63a9bca7') == 0:
-        geteventCmd = 'adb shell getevent -lt /dev/input/event4'   #vivo
+        geteventCmd = 'adb -s %s shell getevent -lt /dev/input/event4' % deviceName   #vivo
     p = Popen(geteventCmd, shell=True, stdout=PIPE)
     print p.pid
     positionRecord = collections.OrderedDict()
@@ -72,7 +81,6 @@ def getevent_position():
     noOutput = False
     hasLine = False
     screenType = check_portrait_landscape()
-    resolution = get_resolution()
     max_width = resolution[0]
     while noOutput is False or hasLine is False:
         if finishFlag is True:
@@ -82,6 +90,7 @@ def getevent_position():
         except Empty:
             noOutput = True
             if hasLine is False:
+                screenType = check_portrait_landscape()
                 time.sleep(0.5)
         else:
             hasLine = True
@@ -151,14 +160,16 @@ def write_constant(scriptFile):
     scriptFile.write('\r')
     scriptFile.write('if __name__ == "__main__":\r')
 
-def generate_script():
+def generate_script(pkName, resolution):
     signal.signal(signal.SIGINT, signal_handler)
     clickRecord = []
     clickNum = 0
+    #resolution = get_resolution()
+
     while finishFlag is False:
         clickNum += 1
         #clickRecord[clickNum] = getevent_position()
-        recordValue = getevent_position()
+        recordValue = getevent_position(resolution)
         if len(recordValue) > 0:
             clickRecord.append(recordValue)
             print '*********第%d次点击' % clickNum
@@ -167,8 +178,11 @@ def generate_script():
     swipeNum = 0
     clickNum = 0
     preClickTime = 0
-    scriptFilePath = os.path.join(scriptFileRoot, '%s-testScript.py' % deviceName)
+    scriptFilePath = os.path.join(scriptFileRoot, '%s.py' % pkName)
+    positionFilePath = os.path.join(scriptFileRoot, '%s.txt' % pkName)
     scriptFile = open(scriptFilePath, 'w')
+    positionFile = open(positionFilePath, 'w')
+    positionFile.write('#screenType[portrait/landscap]:: clickType[tap/swipe]:: position[(x,y)/(x1,y1),(x2,y2)]:: sleepTime\r')
     write_constant(scriptFile)
 
     for record in clickRecord:
@@ -182,28 +196,72 @@ def generate_script():
         if clickType == 'tap':
             print '第%d次点击是: %s, 屏幕状态: %s, 坐标是: %s, 与第%d时间间隔是%s' % (clickNum, clickType, record['screenType'], str(record['clickPosition']), clickNum - 1, str(diffTime))
             if diffTime > 0:
+                printContent = '    print "sleep %s"\r' % str(diffTime)
+                scriptFile.write(printContent)
                 content = '    time.sleep(%s)\r' % str(diffTime)
                 scriptFile.write(content)
-            content = "    os.system('adb -s %s shell input tap %d %d')\r"  % (deviceName, record['clickPosition'][0], record['clickPosition'][1])
+            x_value = record['clickPosition'][0]
+            y_value = record['clickPosition'][1]
+            printContent = "    print 'adb shell input tap %d %d'\r" % (x_value, y_value)
+            scriptFile.write(printContent)
+            content = "    os.system('adb shell input tap %d %d')\r"  % (x_value, y_value)
             scriptFile.write(content)
+            positionContent = '%s:: %s:: (%d, %d):: %s\r' % (record['screenType'], record['clickType'], x_value, y_value, diffTime)
+            positionFile.write(positionContent)
             tapNum += 1
         elif clickType == 'swipe':
             print '第%d次点击是: %s, 屏幕状态: %s, 坐标是: %s%s, 与第%d时间间隔是%s' % (clickNum, clickType, record['screenType'], str(record['clickStartPosition']), str(record['clickEndPosition']), clickNum - 1, str(diffTime))
+            x1_value = record['clickStartPosition'][0]
+            y1_value = record['clickStartPosition'][1]
+            x2_value = record['clickEndPosition'][0]
+            y2_value = record['clickEndPosition'][1]
             if diffTime > 0:
+                printContent = '    print "sleep %s"\r' % str(diffTime)
+                scriptFile.write(printContent)
                 content = '    time.sleep(%s)\r' % str(diffTime)
                 scriptFile.write(content)
-            content = "    os.system('adb -s %s shell input swipe %d %d %d %d')\r" % (deviceName, record['clickStartPosition'][0], record['clickStartPosition'][1], \
-                                    record['clickEndPosition'][0], record['clickEndPosition'][1])
+            printContent = "    print 'adb shell input swipe %d %d %d %d'\r" % (x1_value, y1_value, x2_value, y2_value)
+            scriptFile.write(printContent)
+            content = "    os.system('adb shell input swipe %d %d %d %d')\r" % (x1_value, y1_value, x2_value, y2_value)
             scriptFile.write(content)
+            positionContent = '%s:: %s:: (%d, %d), (%d, %d):: %s\r' % (record['screenType'], record['clickType'], x1_value, y1_value, x2_value, y2_value, diffTime)
+            positionFile.write(positionContent)
             swipeNum += 1
         preClickTime = clickTime
-    content = "    os.system('adb -s %s shell pm clear com.example.TestPlugin')\r" % deviceName
+    printContent = "    print 'sleep 4'\r"
+    scriptFile.write(printContent)
+    content = "    time.sleep(4)\r"   #模拟点击完成后sleep(4s)
     scriptFile.write(content)
+    content = "    os.system('adb shell pm clear com.example.TestPlugin')\r"
+    printContent = "    print 'adb shell pm clear com.example.TestPlugin'\r"
+    scriptFile.write(printContent)
     scriptFile.write(content)
+    scriptFile.write(printContent)
+    scriptFile.write(content)
+    os.system('adb -s %s shell pm clear com.example.TestPlugin' % deviceName)
+    os.system('adb -s %s shell pm clear com.example.TestPlugin' % deviceName)
     scriptFile.write("    print 'end of test script.......'\r")
     scriptFile.close()
     print 'click次数是：%d, 其中tap 次数是: %d, swipe次数是%d' % (clickNum, tapNum, swipeNum)
 
 
 if __name__ == '__main__':
-    generate_script()
+    clearLogCmd = 'adb shell logcat -c'
+    os.system(clearLogCmd)
+    readLogcatCmd = 'adb -s %s logcat -v time' % deviceName
+    proc = subprocess.Popen(readLogcatCmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    pkName = 'noName'
+    resolution = get_resolution()
+    for line in proc.stdout:
+        if 'PluginAutoTest' in line and 'install success' in line:
+            line = line.replace('\r\n', '')
+            splitLine = line.split(': ')
+            pkItem = splitLine[1]
+            pkSplit = pkItem.split()
+            pkName = pkSplit[0]
+            print pkName
+            break
+    proc.kill()
+    proc.wait()
+    print 'kill adb shell logcat thread: %d' % proc.pid
+    generate_script(pkName, resolution)
