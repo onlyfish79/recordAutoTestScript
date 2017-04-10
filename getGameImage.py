efecf572aef9d4ae325048223c761181d21b1732
jinfeng <tianjinfeng@letv.com>
2017-04-10 17:35:29 +0800

    delete unuse code; bugfix and optimize

--- getGameImage.py
+++ getGameImage.py
@@ -1,87 +1,28 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import cv2
-import numpy as np
-import sys
+#import sys
 import os
 import time
-import math
 import subprocess
 from libs.AdbCommand import screencap
 from PIL import Image
 from libs.GetDeviceInfo import getDeviceInfo
 
 currPath = os.getcwd()
-queryImageRoot = os.path.join(currPath, 'queryImage')
-x_reduceRatio = 2   #截图横坐标缩放倍数
-y_reduceRatio = 2   #截图纵坐标缩放倍数
-thumbnailSize = (540.0, 960.0)
+sceneImageRoot = os.path.join(currPath, 'imageFile/sceneImage')
+#thumbnail size = 1.5
+#portrait_thumbnailSize = (720.0, 1280.0)
+#landscape_thumbnailSize = (1280.0, 720.0)
+#thumbnail size = 1.2
+portrait_thumbnailSize = (900.0, 1600.0)
+landscape_thumbnailSize = (1600.0, 900.0)
+#deviceName = 'KWG5T17105003967'
+deviceName = '5LM7N16224000261'
 
-def filter_matches(kp1, kp2, matches, ratio = 0.75):
-    mkp1, mkp2 = [], []
-    for m in matches:
-        if len(m) == 2 and m[0].distance < m[1].distance * ratio:
-            m = m[0]
-            mkp1.append( kp1[m.queryIdx] )
-            mkp2.append( kp2[m.trainIdx] )
-    p1 = np.float32([kp.pt for kp in mkp1])
-    p2 = np.float32([kp.pt for kp in mkp2])
-    kp_pairs = zip(mkp1, mkp2)
-    return p1, p2, list(kp_pairs)
-
-def getImgCordinate(filePath, sceneFilePath, flag):
-    img1 = cv2.imread(filePath)
-    img2 = cv2.imread(sceneFilePath)
-    detector = cv2.AKAZE_create()  #特征识别算法初始化
-    norm = cv2.NORM_HAMMING
-    matcher = cv2.BFMatcher(norm)
-
-    if img1 is None:
-        print 'Failed to load fn1:', filePath
-        sys.exit(1)
-
-    if img2 is None:
-        print 'Failed to load fn2:', sceneFilePath
-        sys.exit(1)
-
-    if detector is None:
-        print 'unknown feature'
-        sys.exit(1)
-
-    print 'using akaze'
-
-    kp1, desc1 = detector.detectAndCompute(img1, None)
-    kp2, desc2 = detector.detectAndCompute(img2, None)
-    print 'matching....'
-    raw_matches = matcher.knnMatch(desc1, trainDescriptors=desc2, k=2) #2特征之匹配
-    p1, p2, kp_pairs = filter_matches(kp1, kp2, raw_matches)
-
-    if len(p1) >= 4:
-        H, status = cv2.findHomography(p1, p2, cv2.RANSAC, 5.0)  #获取转换矩阵
-        print '%d / %d inliers/matched' % (np.sum(status), len(status))
-    else:
-        H, status = None, None
-        print '%d matches found, not enough for homography estimation' % len(p1)
-
-    h1, w1 = img1.shape[:2]
-    h2, w2 = img2.shape[:2]
-    obj_corners = np.float32([[0,0], [w1, 0], [w1, h1], [0, h1]])
-    obj_corners = obj_corners.reshape(1, -1, 2)
-    scene_corners = cv2.perspectiveTransform(obj_corners, H)  #坐标映射
-    scene_corners = scene_corners.reshape(-1, 2)
-    img3 = cv2.rectangle(img2, (int(round(scene_corners[3][0])), int(round(scene_corners[3][1]))), (int(round(scene_corners[1][0])), int(round(scene_corners[1][1]))), (0, 255, 0), 3)
-    resultFilePath = os.path.join(queryImageRoot, pkName, flag+'_match.png')
-    cv2.imwrite(resultFilePath, img3)
-    mid_cordinate_x = int(round((scene_corners[3][0]+scene_corners[1][0])/2))   #计算中心坐标
-    mid_cordinate_y = int(round((scene_corners[3][1]+scene_corners[1][1])/2))
-    #通过特征提取的图片被缩放了，所以计算真正比例的坐标需要在将计算得到的中心坐标在放大reduceRatio
-    return math.floor(mid_cordinate_x*x_reduceRatio), math.floor(mid_cordinate_y*y_reduceRatio)
-
-def thumbnail_pic(path):
+def thumbnail_pic(path, thumbnailSize):
     im = Image.open(path)
     x, y = im.size
-    # thumbnailSize = x/reduceRatio, y/reduceRatio
     im.thumbnail(thumbnailSize)
     savePath = path.replace('.png', '-thumbnail.png')
     print savePath
@@ -90,17 +31,32 @@ def thumbnail_pic(path):
 
 def get_resolution():
     deviceInfo = getDeviceInfo(deviceName)
-    resolutioin = deviceInfo['deviceResolution']
-    splitInfo = resolutioin.split('x')
+    resolution = deviceInfo['deviceResolution']
+    splitInfo = resolution.split('x')
     max_width = int(splitInfo[0].split()[0])
     max_height = int(splitInfo[1].split()[0])
     return (max_width, max_height)
 
+def check_portrait_landscape():
+    checkCmd = "adb -s %s shell dumpsys input | grep 'SurfaceOrientation'  | awk '{ print $2 }' | head -n 1" % deviceName
+    result = os.popen(checkCmd , 'r')
+    content = result.read()
+    result.close()
+    screenType = 'portrait'  #默认竖屏
+    print 'check portrait result is: %s' % repr(content)
+    if len(content) > 0:
+        screenTypeValue = int(content.replace('\r\n', ''))
+        if screenTypeValue == 0 or screenTypeValue == 2:
+            screenType = 'portrait'   #竖屏
+        elif screenTypeValue == 1 or screenTypeValue == 3:
+            screenType = 'landscape'  #横屏
+        else:
+            print 'dumpsys input has problem.......'
+    return screenType
+
 if __name__ == '__main__':
-    global x_reduceRatio, y_reduceRatio
-    queryPicPath = '/Users/helen/Project/autoTest/recordTestScript/queryImage/com.brianbaek.popstar/newGame.png'
-    thumbnail_pic(queryPicPath)
-    sys.exit()
+    x_reduceRatio = 2   #截图横坐标缩放倍数
+    y_reduceRatio = 2   #截图纵坐标缩放倍数
     clearLogCmd = 'adb shell logcat -c'
     os.system(clearLogCmd)
     #readLogcatCmd = 'adb -s %s logcat -v time' % deviceName
@@ -108,15 +64,14 @@ if __name__ == '__main__':
     proc = subprocess.Popen(readLogcatCmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     pkName = 'noName'
     resolution = get_resolution()
-    x_reduceRatio = round(resolution[0]/thumbnailSize[0],2)
-    y_reduceRatio = round(resolution[1]/thumbnailSize[1],2)
     for line in proc.stdout:
-        if 'PluginAutoTest' in line and 'install success' in line:
+        if 'ActivityManager' in line and 'Displayed' in line:
             line = line.replace('\r\n', '')
             splitLine = line.split(': ')
             pkItem = splitLine[1]
             pkSplit = pkItem.split()
-            pkName = pkSplit[0]
+            startInfo = pkSplit[1]
+            pkName = startInfo[0:startInfo.find('/')]
             print pkName
             break
     proc.kill()
@@ -125,30 +80,43 @@ if __name__ == '__main__':
 
     screenCapFlag = raw_input("start to capture: ")
     #为了等待需要截图的画面，如果输入是'cap'的话，则开始截图，否则等待
-    while True:
-        if screenCapFlag == 'start':
-            print 'start to screencap'
-            break
-        else:
-            time.sleep(1)
-            screenCapFlag = raw_input("start to capture: ")
+    #while True:
+    #    if screenCapFlag == 'start':
+    #        print 'start to screencap'
+    #        break
+    #    else:
+    #        time.sleep(1)
+    #        screenCapFlag = raw_input("start to capture: ")
     #如果打开失败，或者是闪退，则输入'stop'退出
     if screenCapFlag != 'stop':
-        queryPkImageRoot = os.path.join(queryImageRoot, pkName)
-        if os.path.isdir(queryPkImageRoot) is False:
-            os.makedirs(queryPkImageRoot)
+        scenePkImageRoot = os.path.join(sceneImageRoot, pkName)
+        if os.path.isdir(scenePkImageRoot) is False:
+            os.makedirs(scenePkImageRoot)
 
-        screenCapName = 'start'  #第一次截图的名字为start，后面的名字根据输入获取
+        #screenCapName = 'start'  #第一次截图的名字为start，后面的名字根据输入获取
+        screenCapName = screenCapFlag
         while True:
             startTime = time.time()
-            #queryImagePath = os.path.join(queryImageRoot, pkName, '%s-enter-part.png' % pkName)
-            sceneFilePath = os.path.join(queryImageRoot, pkName, '%s.png'%screenCapName)
+            sceneFilePath = os.path.join(scenePkImageRoot, '%s.png'%screenCapName)
+            screenType = check_portrait_landscape()
             screencap(sceneFilePath, None)
 
-            #queryImageThumbnailPath = thumbnail_pic(queryImagePath)
-            sceneFileThumbnailPath = thumbnail_pic(sceneFilePath)
+            if cmp(screenType, 'landscape') == 0:
+                thumbnailSize = landscape_thumbnailSize
+            else:
+                thumbnailSize = portrait_thumbnailSize
+            print 'thumbnailSize is %s' % str(thumbnailSize)
 
-            #print getImgCordinate(queryImageThumbnailPath, sceneFileThumbnailPath, 'start')
+            if cmp(screenType, 'landscape') == 0:
+                x_reduceRatio = round(resolution[0]/thumbnailSize[1],2)
+                y_reduceRatio = round(resolution[1]/thumbnailSize[0],2)
+            else:
+                x_reduceRatio = round(resolution[0]/thumbnailSize[0],2)
+                y_reduceRatio = round(resolution[1]/thumbnailSize[1],2)
+            print'resolution is %s, x_reduceRatio is %s, y_reduceRatio is %s' % (str(resolution), str(x_reduceRatio), str(y_reduceRatio))
+
+            sceneFileThumbnailPath = thumbnail_pic(sceneFilePath, thumbnailSize)
+
             endTime = time.time()
             print 'spend time is %s' % str(round(endTime-startTime, 3))
             while True:
