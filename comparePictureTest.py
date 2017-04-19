a8a1c079cffd8bc92772fd563170509dd07907c4
jinfeng <tianjinfeng@letv.com>
2017-04-19 23:53:59 +0800

    优化comparePictureTest.py

--- comparePictureTest.py
+++ comparePictureTest.py
@@ -7,7 +7,10 @@ import sys
 import os
 import time
 import math
+import traceback
+from fractions import Fraction
 from PIL import Image
+from libs.GetDeviceInfo import getDeviceInfo
 
 currPath = os.getcwd()
 queryImageRoot = os.path.join(currPath, 'imageFile/queryImage')
@@ -15,6 +18,12 @@ sceneImageRoot = os.path.join(currPath, 'imageFile/sceneImage')
 matchImageRoot = os.path.join(currPath, 'imageFile/matchImage')
 reduceRatio = 1   #截图缩放倍数
 #reduceRatio = 1.6
+
+#deviceName = '5LM7N16224000261'
+#deviceName = 'KWG5T17105003967'  #hw P9
+#deviceName = '63a9bca7'  #vivo
+deviceName = 'LGH8689e43a709'  #LG
+#deviceName = '635f9505'    #MI 5
 
 def filter_matches(kp1, kp2, matches, ratio = 0.75):
     mkp1, mkp2 = [], []
@@ -104,25 +113,67 @@ def getImgCordinate(filePath, sceneFilePath, flag):
     mid_cordinate_x = int(round((scene_corners[3][0]+scene_corners[1][0])/2))   #计算中心坐标
     mid_cordinate_y = int(round((scene_corners[3][1]+scene_corners[1][1])/2))
     #通过特征提取的图片被缩放了，所以计算真正比例的坐标需要在将计算得到的中心坐标在放大reduceRatio
-    return math.floor(mid_cordinate_x*reduceRatio), math.floor(mid_cordinate_y*reduceRatio)
+    x_fraction = mid_cordinate_x * x_reduceRatio
+    y_fraction = mid_cordinate_y * y_reduceRatio
+    real_x = math.floor(x_fraction.numerator/float(x_fraction.denominator))
+    real_y = math.floor(y_fraction.numerator/float(y_fraction.denominator))
+    #print 'real x: %d, real y: %d' % (real_x, real_y)
+    if real_x < 0 or real_y < 0:
+        return None, None
+    return real_x, real_y
 
-def thumbnail_pic(path):
-    im = Image.open(path)
-    x, y = im.size
-    print '%s, w=%d,h=%d' % (path, x, y)
-    thumbnailSize_x = int(math.ceil(x/reduceRatio))
-    thumbnailSize_y = int(math.ceil(y/reduceRatio))
-    thumbnailSize = (thumbnailSize_x, thumbnailSize_y)
-    print thumbnailSize
-    im.thumbnail(thumbnailSize)
-    savePath = path.replace('.png', '-thumbnail.png')
-    #im.resize(thumbnailSize, Image.ANTIALIAS).save(savePath)
-    im.save(savePath)
+def thumbnail_pic(path, thumbnailSize):
+    savePath = None
+    try:
+        im = Image.open(path)
+        x, y = im.size
+        #thumbnailSize = x/reduceRatio, y/reduceRatio
+        im.thumbnail(thumbnailSize)
+        savePath = path.replace('.png', '-thumbnail.png')
+        im.save(savePath)
+    except:
+        print 'thumbnail_pic catch exception: %s' % str(traceback.format_exc())
     return savePath
 
+#小米5S检测横竖屏的时候，返回两行
+#SurfaceOrientation: 0, 第一行结果一直是0
+#SurfaceOrientation: 0/1, 第二行，横屏为1，竖屏为0
+def check_portrait_landscape():
+    checkCmd = "adb -s %s shell dumpsys input | grep 'SurfaceOrientation'  | awk '{ print $2 }'" % deviceName
+    result = os.popen(checkCmd , 'r')
+    content = result.readlines()
+    result.close()
+
+    screenType = 'portrait'  #默认竖屏
+    print 'check portrait result is: %s' % repr(content)
+    if len(content) > 0:
+        orientationValue = content[len(content)-1].replace('\r\n', '')
+        screenTypeValue = int(orientationValue)
+        if screenTypeValue == 0 or screenTypeValue == 2:
+            screenType = 'portrait'   #竖屏
+        elif screenTypeValue == 1 or screenTypeValue == 3:
+            screenType = 'landscape'  #横屏
+        else:
+            print 'dumpsys input has problem.......'
+    return screenType
+
+def get_resolution():
+    deviceInfo = getDeviceInfo(deviceName)
+    resolutioin = deviceInfo['deviceResolution']
+    splitInfo = resolutioin.split('x')
+    max_width = int(splitInfo[0].split()[0])
+    max_height = int(splitInfo[1].split()[0])
+    return (max_width, max_height)
 
 if __name__ == '__main__':
     pkName = 'com.babeltime.fknsg2.qihoo'
+    resolution = get_resolution()
+    if resolution[0] == 1080 and resolution[1] == 1920:
+        x_reduceRatio = 1
+        y_reduceRatio = 1
+    else:
+        x_reduceRatio = Fraction(4, 3)   #截图横坐标缩放倍数4/3
+        y_reduceRatio = Fraction(4, 3)   #截图纵坐标缩放倍数
 
     queryPkImageRoot = os.path.join(queryImageRoot, pkName)
     matchPkImageRoot = os.path.join(matchImageRoot, pkName)
@@ -133,6 +184,18 @@ if __name__ == '__main__':
         os.makedirs(matchPkImageRoot)
     if os.path.isdir(scenePkImageRoot) is False:
         os.makedirs(scenePkImageRoot)
+    screenType = check_portrait_landscape()
+    #因为此处已经知道计算后的结果是整数，所以缩放后的值就是分子
+    if cmp(screenType, 'landscape') == 0:
+        thumbnail_x = resolution[1]/x_reduceRatio
+        thumbnail_y = resolution[0]/y_reduceRatio
+        thumbnailSize = (thumbnail_x.numerator, thumbnail_y.numerator) #landscape_thumbnailSize
+    else:
+        thumbnail_x = resolution[0]/x_reduceRatio
+        thumbnail_y = resolution[1]/y_reduceRatio
+        thumbnailSize = (thumbnail_x.numerator, thumbnail_y.numerator) #portrait_thumbnailSize
+    print 'thumbnailSize is %s' % str(thumbnailSize)
+    print 'resolution is %s, x_reduceRatio is %s, y_reduceRatio is %s' % (str(resolution), str(x_reduceRatio), str(y_reduceRatio))
     startTime = time.time()
     #queryImagePath = os.path.join(queryPkImageRoot, 'IKnown.png')
     #sceneFilePath = os.path.join(scenePkImageRoot, 'IKnown.png')
@@ -142,8 +205,12 @@ if __name__ == '__main__':
     #sceneFileThumbnailPath = sceneFilePath
     #queryImageThumbnailPath = '/Users/helen/Desktop/clickCross2.png'
     #sceneFileThumbnailPath = '/Users/helen/Desktop/clickCross2 2.png'
-    queryImageThumbnailPath = '/Users/helen/Desktop/IKnown 2.png'
-    sceneFileThumbnailPath = '/Users/helen/Desktop/IKnown.png'
+    queryImageThumbnailPath = '/Users/helen/Desktop/hostPluginMainPage.png'
+    sceneFilePath = '/Users/helen/Desktop/hostMain.png'
+    if resolution[0] == 1080 and resolution[1] == 1920:
+        sceneFileThumbnailPath = sceneFilePath
+    else:
+        sceneFileThumbnailPath = thumbnail_pic(sceneFilePath, thumbnailSize)
 
     print getImgCordinate(queryImageThumbnailPath, sceneFileThumbnailPath, 'start')
     endTime = time.time()
